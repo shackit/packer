8818f77e9a1d13ee5d34516aa0b470b591f53bcd
shackit <hackett_s@hotmail.com>
2017-03-27 11:07:13 +0100

    removes windows

--- scripts/windows/vm-tools.bat
+++
@@ -1,9 +0,0 @@
-if "%PACKER_BUILDER_TYPE%" equ "virtualbox-iso" goto :virtualbox
-
-:virtualbox
-
-:: There needs to be Oracle CA (Certificate Authority) certificates installed in order
-:: to prevent user intervention popups which will undermine a silent installation.
-cmd /c certutil -addstore -f "TrustedPublisher" A:\oracle-cert.cer
-
-cmd /c E:\VBoxWindowsAdditions.exe /S
